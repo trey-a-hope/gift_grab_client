eaddbf41fc8ec195c6b682a83983e6f7a9ffeeab
Trey Hope <trey.a.hope@gmail.com>
2025-08-03 20:18:55 -0500

    Update dependencies and refactor project structure
    - Migrated `gift_grab_ui` and `gift_grab_game` dependencies to use Git URLs for better version control.
    - Added several new dependencies including `flutter_secure_storage`, `equatable`, `flutter_login`, `go_router`, `flutter_bloc`, and others to enhance functionality.
    - Implemented Apple Sign-In functionality in `main.dart` with a new method to retrieve the Apple ID token.
    - Cleaned up commented paths in `pubspec.yaml` to streamline the configuration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:gift_grab_game/game/gift_grab_game_widget.dart';
 import 'package:gift_grab_ui/ui.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+
+Future<String?> getAppleToken() async {
+  try {
+    final credential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+    );
+
+    return credential.identityToken;
+  } on SignInWithAppleException catch (e) {
+    if (e.toString().contains('canceled')) {
+      return null;
+    }
+    rethrow;
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final token = await getAppleToken();
 
   runApp(const MyAppPage());
 }
