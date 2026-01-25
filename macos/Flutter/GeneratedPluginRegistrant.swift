504a4c39f88b9b7b3093f72e17f8efed01883b86
Trey Hope <trey.a.hope@gmail.com>
2026-01-24 22:23:05 -0600

    build: Update Flutter package dependencies, bump minimum iOS version to 13.0, and refresh CocoaPods configuration.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,10 +6,10 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import flutter_libphonenumber_darwin
 import flutter_secure_storage_macos
 import google_sign_in_ios
 import package_info_plus
-import path_provider_foundation
 import screen_retriever_macos
 import sign_in_with_apple
 import url_launcher_macos
@@ -17,10 +17,10 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FlutterLibphonenumberPlugin.register(with: registry.registrar(forPlugin: "FlutterLibphonenumberPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
